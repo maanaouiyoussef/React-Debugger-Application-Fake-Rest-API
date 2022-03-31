dd84565c1bf42bc26abb681a1077e0ab8b9c89e0
youssefmaanaoui <yussefmaanaoui@gmail.com>
2022-04-01 00:14:13 +0200

    new bug

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({text, bg, onAdd}) => {
     return (
         <div>
-            <button onClick={onAdd} className="btn" style={{backgroundColor: bg}}>{text}</button>
+            <button onClick={onAdd} className="btn" style={{backgroundColor: bg, color: 'white'}}>{text}</button>
         </div>
     )
 }
